c9328b4e7e4af70240a2651cf1bdc56ac2c79e46
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2022-08-26 23:46:47 +0200

    Added check for numbers in row and cols

--- learning_np.py
+++ learning_np.py
@@ -11,6 +11,31 @@ puzzle = np.array([[0, 0, 0, 0, 6, 0, 2, 4, 3],
                     [0, 0, 0, 6, 0, 1, 3, 2, 0],
                     [2, 3, 8, 0, 9, 0, 0, 0, 0]])
 
-print(puzzle)
-print(puzzle.ndim)
-print(puzzle.size)
+# print(puzzle)
+# print(puzzle.ndim)
+# print(puzzle.size)
+
+test_row = 0
+test_col = 4
+complete = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# # row first then column for indexing
+# # index using comma instead of 2 pairs of square brackets
+# print(puzzle[test_row, test_col])
+
+# printing 5th column: index=4
+# print(np.array(puzzle[:9, test_col:test_col+1]).flatten())
+
+for row_index, row in enumerate(puzzle, start=1):
+    for col, cell in enumerate(row, start=1):
+        if cell != 0:
+            pass
+
+        else:
+            column = puzzle[:9, col-1:col].flatten()
+            look_row_and_col = np.union1d(row, column)
+            candidates = np.setdiff1d(complete, look_row_and_col)
+            if len(candidates) == 2:
+                print(f"Row {row_index}: {row}")
+                print(f"Column {col}: {column}")
+                print(f"Candidates: {candidates}")
